5e76dde059dae2308d09b7b526bd2334cf7b8efa
salteax <salteax1@protonmail.com>
2023-12-03 13:40:43 +0100

    Umsetzung GUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+// Piston
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateEvent};
+use piston::window::WindowSettings;
+
+// Rest
 use serde::Deserialize;
 use core::time;
 use std::{fs, thread::sleep};
@@ -46,36 +59,6 @@ fn init_field() -> Vec<Vec<bool> > {
     game_grid
 }
 
-fn print_game_grid(game_grid: &Vec<Vec<bool>>) {
-    // Screen leeren und Cursor auf Position 1 1
-    print!("\x1B[2J\x1b[1;1H");
-    
-    // Farben
-    let alive_color = "\x1B[32m";
-    let dead_color = "\x1B[31m";
-    let reset_color = "\x1B[0m";
-
-    // Zeichne oberer Rahmen
-    let width = game_grid[0].len() * 2;
-    println!("+{}+", "-".repeat(width));
-
-    // Grid ausgeben
-    for row in game_grid {
-        print!("|");
-        for &cell in row {
-            if cell {
-                print!("{}x{} ", alive_color, reset_color);
-            } else {
-                print!("{}.{} ", dead_color, reset_color);
-            }
-        }
-        println!("|");
-    }
-
-    // Zeichne unterer Rahmen
-    println!("+{}+", "-".repeat(width));
-}
-
 fn next_gen(grid: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     // Config
     let config = &*CONFIG;
@@ -102,14 +85,6 @@ fn next_gen(grid: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
 
             if !*cell && alive==3 {next[y-1][x-1]=true;}
             if *cell && alive>1 && alive<4 {next[y-1][x-1]=true;}
-
-            
-            /*print!("{}",x);
-            print!(" ");
-            print!("{}",y);
-            print!(" ");
-            print!("{}",alive);
-            println!();*/
         }
     }
 
@@ -128,16 +103,79 @@ fn check(mut x: usize, mut y: usize, grid: &Vec<Vec<bool>>) -> bool {
     return grid[y-1][x-1];
 }
 
+pub struct Game {
+    gl: GlGraphics,
+}
+
+impl Game {
+    fn render(&mut self, args: &RenderArgs, game_grid: &Vec<Vec<bool>>) {
+        use graphics::*;
+
+        const ALIVE_COLOR: [f32; 4] = [0.0, 1.0, 0.0, 1.0]; // Grün
+        const DEAD_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0]; // Grau
+        const GRID_COLOR: [f32; 4] = [0.2, 0.2, 0.2, 1.0]; // Gitterfarbe
+        const CELL_SIZE: f64 = 25.0;
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(DEAD_COLOR, gl);
+
+            for (i, row) in game_grid.iter().enumerate() {
+                for (j, &cell) in row.iter().enumerate() {
+                    let color = if cell { ALIVE_COLOR } else { DEAD_COLOR };
+
+                    let x = (j as f64) * CELL_SIZE;
+                    let y = (i as f64) * CELL_SIZE;
+
+                    let square = rectangle::square(x, y, CELL_SIZE);
+                    let transform = c.transform;
+
+                    rectangle(color, square, transform, gl);
+                }
+            }
+
+            let transform = c.transform;
+
+            for i in 0..game_grid.len() + 1 {
+                let y = (i as f64) * CELL_SIZE;
+                line(GRID_COLOR, 0.5, [0.0, y, args.window_size[0], y], transform, gl);
+            }
+
+            for j in 0..game_grid[0].len() + 1 {
+                let x = (j as f64) * CELL_SIZE;
+                line(GRID_COLOR, 0.5, [x, 0.0, x, args.window_size[1]], transform, gl);
+            }
+        });
+    }
+}
+
 fn main() {
+    let opengl = OpenGL::V3_2;
+
+    // Gamegrid init
     let mut game_grid = init_field();
-    let mut i = 1;
-    loop {
-        print_game_grid(&game_grid);
-        print!("Generation: ");
-        print!("{}",i);
-        println!();
-        game_grid = next_gen(&game_grid);
-        i+=1;
-        sleep(time::Duration::from_millis(300));
+    
+    // Create a Glutin window.
+    let config = &*CONFIG;
+
+    let mut window: Window = WindowSettings::new::<&str, (u32, u32)>("Game of Life", (config.grid_size.width as u32 * 25, config.grid_size.height as u32 * 25).into())
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = Game{
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args, &game_grid);
+        }
+
+        if let Some(args) = e.update_args() {
+            game_grid = next_gen(&game_grid);
+            sleep(time::Duration::from_millis(150));
+        }
     }
 }
